0f8ab3f56e8a200a141d9aa0d01653bcd2467a5f
colin <nicktheodoulou>
2020-03-22 19:22:16 +0000

    - debugged another error

--- siers_model_disease.py
+++ siers_model_disease.py
@@ -48,21 +48,21 @@ def add(l1, l2):
 def main():
 
     parameters = {
-        "birth_rate": 0.001,
-        "death_rate": 0.0005,
-        "rate_of_transmission": 0.7,
+        "birth_rate": 0.00,
+        "death_rate": 0.00,
+        "rate_of_transmission": 0.96,
         "rate_of_incubation": 1,
         "rate_of_recovery": 0.90,
-        "rate_of_loss_of_immunity": 0.1,
+        "rate_of_loss_of_immunity": 0.003,
         "rate_of_vaccination": 0,
         "rate_of_treatment": 0
 
     }
 
     pop_susceptible = 50000000  # number susceptible: Not yet infected.
-    pop_infectious = 200  # number infected: Ongoing disease.
-    pop_recovered = 0  # number recovered: Gained immunity, isolated, or dead.
-    pop_exposed = 0  # number exposed
+    pop_infectious = 500  # number infected: Ongoing disease.
+    pop_recovered = 100  # number recovered: Gained immunity, isolated, or dead.
+    pop_exposed = 5000000  # number exposed
     vector_population = [pop_susceptible, pop_exposed, pop_infectious, pop_recovered]
 
     # Create graphs.
@@ -90,6 +90,7 @@ def main():
         e_plot.plot(time, vector_population[1])
         i_plot.plot(time, vector_population[2])
         r_plot.plot(time, vector_population[3])
+
 
         time += dt
 
