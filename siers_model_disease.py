fb7e08c872b9f984e3c9687cc0850f3a0c53cae1
colin <nicktheodoulou>
2020-03-22 16:18:17 +0000

    - converted parameters to dictionary

--- siers_model_disease.py
+++ siers_model_disease.py
@@ -6,34 +6,29 @@ import vpython
 # https://sci-hub.tw/10.1007/s10483-007-0914-x
 
 
-def derivs(t, population):
-
-    # Set initial conditions.
-
+def derivs(t, population, parameters):
     # parameters
-    birth_rate = 0.000001  # birth rate
-    death_rate = 0.0000005  # death rate
-    rate_of_transmission = 0.8  # rate of transmission
-    rate_of_incubation = 1.0  # the rate of latent individuals becoming infectious (average duration of incubation is 1/\sigma)
-    rate_of_recovery = 0.90  # the rate of cure of disease
-    rate_of_loss_of_immunity = 0.01  # the rate at which recovered individuals return to the susceptible statue due to loss of immunity.
-    rate_of_vaccination = 0  # the ratio of the number of individuals who received the vaccine
-    rate_of_treatment = 0  # giving treatment
+    b = parameters['birth_rate']  # birth rate
+    mu = parameters['death_rate']  # death rate
+    beta = parameters['rate_of_transmission']  # rate of transmission
+    alpha = parameters['rate_of_incubation']  # the rate of latent individuals becoming infectious (average duration of incubation is 1/\sigma)
+    gamma = parameters['rate_of_recovery']  # the rate of cure of disease
+    sigma = parameters['rate_of_loss_of_immunity']  # the rate at which recovered individuals return to the susceptible statue due to loss of immunity.
+    nu = parameters['rate_of_vaccination']  # the ratio of the number of individuals who received the vaccine
+    q = parameters['rate_of_treatment']  # giving treatment
 
+    s = population[0]  # number susceptible: Not yet infected.
+    e = population[1]  # number exposed
+    i = population[2]  # number infected: Ongoing disease.
+    r = population[3]  # number recovered: Gained immunity, isolated, or dead.
+    total_population = s + e + i + r
 
+    ds_dt = (b * total_population) + (sigma * r) - (beta * s * i / total_population) - (nu * s) - (mu * s)
+    de_dt = (beta * s * i / total_population) - (alpha * e) - (mu * e)
+    di_dt = (alpha * e) + (q * i) - (gamma * i) - (mu * i)
+    dr_dt = (gamma * i) + (nu * s) - (sigma * r) - (mu * r)
 
-    S = population[0]  # number susceptible: Not yet infected.
-    I = population[1]  # number infected: Ongoing disease.
-    R = population[2]  # number recovered: Gained immunity, isolated, or dead.
-    E = population[3]  # number exposed
-    total_population = S + I + R + E
-
-    Sdot = (birth_rate * total_population) + (rate_of_loss_of_immunity * R) - (rate_of_transmission * S * I / total_population) - (rate_of_vaccination * S) - (death_rate * S)
-    Edot = (rate_of_transmission * S * I / total_population) - (rate_of_incubation * E) - (death_rate * E)
-    Idot = (rate_of_incubation * E) + (rate_of_treatment * I) - (rate_of_recovery * I) - (death_rate * I)
-    Rdot = (rate_of_recovery * I) + (rate_of_vaccination * S) - (rate_of_loss_of_immunity * R) - (death_rate * R)
-
-    return [Sdot, Idot, Rdot, Edot]
+    return [ds_dt, de_dt, di_dt, dr_dt]
 
 
 def mult(l1, scal):
@@ -52,11 +47,23 @@ def add(l1, l2):
 
 def main():
 
+    parameters = {
+        "birth_rate": 0.000001,
+        "death_rate": 0.0000005,
+        "rate_of_transmission": 0.8,
+        "rate_of_incubation": 1.0,
+        "rate_of_recovery": 0.90,
+        "rate_of_loss_of_immunity": 0.01,
+        "rate_of_vaccination": 0,
+        "rate_of_treatment": 0
+
+    }
+
     pop_susceptible = 50000000  # number susceptible: Not yet infected.
     pop_infectious = 500000  # number infected: Ongoing disease.
     pop_recovered = 1000  # number recovered: Gained immunity, isolated, or dead.
     pop_exposed = 5000000  # number exposed
-    vector_population = [pop_susceptible, pop_infectious, pop_recovered, pop_exposed]
+    vector_population = [pop_susceptible, pop_exposed, pop_infectious, pop_recovered]
 
     # Create graphs.
     s_plot = vpython.gcurve(color=vpython.color.blue, label="% susceptible")
@@ -70,13 +77,11 @@ def main():
 
     while time < tmax:
         # Use RK4 method to determine the new vector_population.
-        k1 = derivs(time, vector_population)
-        k2 = derivs(time + dt / 2, add(vector_population, mult(k1, 0.5)))
-        k3 = derivs(time + dt / 2, add(vector_population, mult(k2, 0.5)))
-        k4 = derivs(time + dt, add(vector_population, k3))
-        vector_population = add(vector_population,
-                                mult(add(mult(k1, 1 / 6), add(mult(k2, 1 / 3), add(mult(k3, 1 / 3), mult(k4, 1 / 6)))),
-                                     dt))
+        k1 = derivs(time, vector_population, parameters)
+        k2 = derivs(time + dt / 2, add(vector_population, mult(k1, 0.5)), parameters)
+        k3 = derivs(time + dt / 2, add(vector_population, mult(k2, 0.5)), parameters)
+        k4 = derivs(time + dt, add(vector_population, k3), parameters)
+        vector_population = add(vector_population, mult(add(mult(k1, 1 / 6), add(mult(k2, 1 / 3), add(mult(k3, 1 / 3), mult(k4, 1 / 6)))), dt))
         print(vector_population)
 
         # plot using vpython
